8667fc05fb0d1d3a9d5ee801c2906503d1ec0b98
user-c-2001 <2821910942@qq.com>
2022-08-22 15:07:57 +0800

    公共顶部完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 class indexHtml{
     constructor(){
         this.$Navigation = $('#Navigation');
+        this.$Search = $('#Search');
         this.init();
     }
     init(){
         this.$Navigation.load('./html/public.html #p-Navigation');
+        this.$Search.load('./html/public.html #p-Search');
     }
 }
 $(function(){
