330e2b01dd9b775c186eb810a262365a5c2e918c
user-c-2001 <2821910942@qq.com>
2022-08-23 11:57:08 +0800

    侧边部分基本完成

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,13 @@ class indexHtml{
     constructor(){
         this.$Navigation = $('#Navigation');
         this.$Search = $('#Search');
+        this.$Sidelist = $('#Sidelist');
         this.init();
     }
     init(){
         this.$Navigation.load('./html/public.html #p-Navigation');
         this.$Search.load('./html/public.html #p-Search');
+        this.$Sidelist.load('./html/public.html #p-Sidelist');
     }
 }
 $(function(){
